4bb10a25cda810d02a19c0c6c3216aaef60ea2d8
Olexiy Konstantynov <konstantynov.olexiy@gmail.com>
2024-05-13 19:24:06 +0300

    Added the project and the software

---
+++ lab10/prj/TestDriver/TestDriver/TestDriver.cpp
@@ -0,0 +1,13 @@
+﻿#include <string>;
+#include "../../../../lab08/prj/ModulesKonstantynov/ModulesKonstantynov.h"
+
+
+
+int main() {
+	std::string testf = "Test.txt";
+	task1(testf);
+	task2(testf);
+	task3(testf,10,20,30,10);
+
+	return 0;
+}
