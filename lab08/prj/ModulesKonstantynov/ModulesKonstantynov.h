ad8c1d39d441bfb56c1c803c43b48ce87abe26a8
Olexiy Konstantynov <konstantynov.olexiy@gmail.com>
2024-05-13 19:25:20 +0300

    updated the modules

--- lab08/prj/ModulesKonstantynov/ModulesKonstantynov.h
+++ lab08/prj/ModulesKonstantynov/ModulesKonstantynov.h
@@ -1,5 +1,9 @@
 #pragma once
+#include <string>;
 float s_calculation(float, float, float);
 int getGrade(float);
 int temperature(int);
 int primalNum(int);
+void task1(std::string);
+void task2(std::string);
+void task3(std::string, int, int, int, int);
--- lab08/prj/ModulesKonstantynov/main.cpp
+++ lab08/prj/ModulesKonstantynov/main.cpp
@@ -1,6 +1,15 @@
 #define _USE_MATH_DEFINES
-#include<cmath>
+#define _CRT_SECURE_NO_WARNINGS
+#include <cmath>
 #include <bitset>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <Windows.h>
+#include <list>
+
+
+using namespace std;
 
 float s_calculation(float x, float y, float z) {
 	float s = log(fabs(sin(z))) + (0.5 * x * x - pow(fabs((pow((y + z), 2)) - pow(x, 5)), 0.5) / (10 * M_PI * pow(z, 4)));
@@ -59,4 +68,83 @@ int primalNum(int number) {
 		}
 	}
 	
+}
+
+void task1(string file) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char c;
+	fstream fs;
+	fs.open(file, fstream::in | fstream::out);
+	if (!fs.is_open()) {
+		cout << "Помилка відкриття файлу";
+	}
+	else {
+		list<string> text;
+		string paragraph, language;
+		while (getline(fs, paragraph)) {
+			text.push_back(paragraph);
+		}
+		text.reverse();		
+		fs.seekg(0);
+		while (fs.get(c)) {
+			if ((c >= (char)0xC0 && c <= (char)0xDF) || (c >= (char)0xE0 && c <= (char)0xFF)) {
+				language = "Текст у файлі українською мовою\n";
+				break;
+			}
+			else if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+				language = "Текст у файлі англійською мовою\n";
+				break;
+			}
+		}
+		fs << "Модуль розробив Олексій Константинов. ЦНТУ, Кропивницький, Україна, 2024\n";
+		for (const auto& str : text) {
+			fs << str << endl;
+		}
+		fs << language;
+		fs.close();
+	}
+}
+
+void task2(string file) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	fstream fs;
+	int counter = 0;
+	char c;
+	fs.open(file, fstream::in | fstream::out | fstream::app);
+	if (!fs.is_open()) {
+		cout << "Помилка відкриття файлу";
+	}
+	else {
+		while (fs.get(c)) {
+			if (c == '.' || c == ',') {
+				counter++;
+			}
+		}
+		time_t current_time = time(nullptr);
+		tm* local_time = localtime(&current_time);
+		fs << "Кількість \",\" і \".\" у тексті:" << counter << endl << "Дата та час дозапису: " << asctime(local_time) << endl;
+		fs.close();
+	}
+}
+
+void task3(string file, int x, int y, int z, int b) {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string bin;
+	fstream fs;
+	fs.open(file, fstream::in | fstream::out | fstream::app);
+	if (!fs.is_open()) {
+		cout << "Помилка відкриття файлу";
+	}
+	else {
+		fs << s_calculation(x, y, z);
+		while (b > 0) {
+			bin = to_string(b % 2) + bin;
+			b /= 2;
+		}
+		fs << bin;
+		fs.close();
+	}
 }
